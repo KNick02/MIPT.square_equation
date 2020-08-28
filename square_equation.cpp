6db56f9de8453926f7a25b60be9f87e96cad7b69
KNick02 <70335914+KNick02@users.noreply.github.com>
2020-08-28 14:12:14 +0300

    Update square_equation.cpp

--- square_equation.cpp
+++ square_equation.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 #define INF_ROOTS 3
+#define ZERO 1.0E-20
 
 
 //-----------------------------------------------------------------------------
@@ -16,92 +17,6 @@ int print_intro()
     printf("Square equation solver\n");
 
     printf("Enter coefficients: ");
-
-    return 0;
-    }
-
-
-//-----------------------------------------------------------------------------
-//! Solves a linear equation ex + f = 0
-//! @param [in]      e             first coefficient
-//! @param [in]      f             second coefficient
-//! @param [out]     x1            root value
-//! @param [out]     number_roots  number of roots
-//-----------------------------------------------------------------------------
-
-
-int lin_eq(double e, double f, double* x1, int* number_roots)
-    {
-    assert(x1 != NULL);
-    assert(number_roots != NULL);
-
-    assert(isfinite(e));
-    assert(isfinite(f));
-
-    if (e != 0)
-        {
-        *x1 = -f / e;
-        *number_roots = 1;
-        }
-
-    else
-        {
-        if (f != 0)
-            *number_roots = 0;
-
-        else
-            *number_roots = INF_ROOTS;
-        }
-    return 0;
-    }
-
-
-//-----------------------------------------------------------------------------
-//! Solves a square equation ax**2 + bx + c = 0
-//!
-//! @param [in]      a             a-coefficient
-//! @param [in]      b             b-coefficient
-//! @param [in]      c             c-coefficient
-//! @param [out]     x1            first root value
-//! @param [out]     x2            second root value
-//! @param [out]     number_roots  number of roots
-//-----------------------------------------------------------------------------
-
-int find_roots(double a, double b, double c, double* x1, double* x2, int* number_roots)
-    {
-    assert(x1 != NULL);
-    assert(x2 != NULL);
-    assert(number_roots != NULL);
-
-    assert(isfinite(a));
-    assert(isfinite(b));
-    assert(isfinite(c));
-
-    double d;
-
-    if (a != 0)
-        {
-        d = b*b - 4*a*c;
-
-        if (d > 0)
-            {
-            *x1 = (-b + sqrt(d)) / (2*a);
-            *x2 = (-b - sqrt(d)) / (2*a);
-            *number_roots = 2;
-            }
-
-        else if (d == 0)
-            {
-            *x1 = -b / (2*a);
-            *number_roots = 1;
-            }
-
-        else
-            *number_roots = 0;
-        }
-
-    else
-        lin_eq(b, c, x1, number_roots);
 
     return 0;
     }
@@ -140,6 +55,111 @@ int print_answer(double x1, double x2, int number_roots)
     }
 
 
+//-----------------------------------------------------------------------------
+//! Checks if a number is equal to zero with less precision
+//!
+//! @param [in]   y   number value
+//!
+//! @return  Returns 1 if the number is equal to zero and 0 if it is not
+//-----------------------------------------------------------------------------
+
+
+int checkZero(double y)
+    {
+    if ((y > ZERO) || (y < -ZERO))
+        return 0;
+
+    else
+        return 1;
+    }
+
+
+//-----------------------------------------------------------------------------
+//! Solves a linear equation ex + f = 0
+//! @param [in]      e             first coefficient
+//! @param [in]      f             second coefficient
+//! @param [out]     x1            root value
+//! @param [out]     number_roots  number of roots
+//-----------------------------------------------------------------------------
+
+
+int lin_eq(double e, double f, double* x1, int* number_roots)
+    {
+    assert(x1 != NULL);
+    assert(number_roots != NULL);
+
+    assert(isfinite(e));
+    assert(isfinite(f));
+
+    if (checkZero(e) == 0)
+        {
+        *x1 = -f / e;
+        *number_roots = 1;
+        }
+
+    else
+        {
+        if (checkZero(f) == 0)
+            *number_roots = 0;
+
+        else
+            *number_roots = INF_ROOTS;
+        }
+    return 0;
+    }
+
+
+//-----------------------------------------------------------------------------
+//! Solves a square equation ax**2 + bx + c = 0
+//!
+//! @param [in]      a             a-coefficient
+//! @param [in]      b             b-coefficient
+//! @param [in]      c             c-coefficient
+//! @param [out]     x1            first root value
+//! @param [out]     x2            second root value
+//! @param [out]     number_roots  number of roots
+//-----------------------------------------------------------------------------
+
+
+int find_roots(double a, double b, double c, double* x1, double* x2, int* number_roots)
+    {
+    assert(x1 != NULL);
+    assert(x2 != NULL);
+    assert(number_roots != NULL);
+
+    assert(isfinite(a));
+    assert(isfinite(b));
+    assert(isfinite(c));
+
+    double d = 0;
+
+    if (checkZero(a) == 0)
+        {
+        d = b*b - 4*a*c;
+
+        if (d > ZERO)
+            {
+            *x1 = (-b + sqrt(d)) / (2*a);
+            *x2 = (-b - sqrt(d)) / (2*a);
+            *number_roots = 2;
+            }
+
+        else if (d < -ZERO)
+            *number_roots = 0;
+
+        else
+            {
+            *x1 = -b / (2*a);
+            *number_roots = 1;
+            }
+        }
+
+    else
+        lin_eq(b, c, x1, number_roots);
+
+    return 0;
+    }
+
 
 int main()
     {
@@ -148,10 +168,10 @@ int main()
     double a = 0, b = 0, c = 0;
     int check_input;
 
-    if ((check_input = scanf("%lg %lg %lg", &a, &b, &c)) != 3)
+    while ((check_input = scanf("%lg %lg %lg", &a, &b, &c)) != 3)
         {
-        printf("Error. a, b and c must be numbers");
-        return 1;
+        printf("Try again (a, b and c must be numbers): ");
+        fflush(stdin);
         }
 
     double x1 = 0, x2 = 0;
@@ -163,5 +183,6 @@ int main()
 
     return 0;
     }
+
 
 
